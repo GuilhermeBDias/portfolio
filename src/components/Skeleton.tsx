b63e3cb58791f1302407bd98332c1ea2bac266bc
GuilhermeBDias <guilhermebdias55@gmail.com>
2025-09-12 13:00:37 -0300

    feat: Skeleton em andamento

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,6 +1,8 @@
 
 export const Skeleton = () => {
   return (
-    <div className="flex w-full h-full z-10 justify-end">Skeleton</div>
+    <div className="flex w-full h-full z-10 justify-end">
+      <div className="flex flex-col w-[50%] bg-[#0C0E23]">Skeleton</div>
+    </div>
   )
 }
