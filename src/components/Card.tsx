0ca36b8044293d606b67bdd3404766a7815eb6e9
GuilhermeBDias <guilhermebdias55@gmail.com>
2025-09-16 09:59:36 -0300

    feat: animação adicional na navBar

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,32 @@
+import { GoArrowUpRight } from "react-icons/go";
 import type { CardProps } from "../utils/Types";
 
-export const Card = ({ title, description} : CardProps) => {
+export const Card = ({ title, description }: CardProps) => {
   return (
     <div className="flex flex-col w-full h-full bg-[#04071D] p-4">
       <div className="flex relative justify-center items-center overflow-hidden w-full h-[60%] bg-[#13162D] rounded-2xl">
         <div className="absolute left-30 -top-20 w-[30%] h-[100%] rotate-45 rounded-full bg-radial-[at_40%_85%] from-cyan-600/80 via-gray-600/50 to-violet-800/90 blur-3xl" />
         {/* imagem aqui */}
       </div>
-      <h1 className="w-full p-2 md:p-4 text-xl md:text-3xl font-bold">{title}</h1>
-      <p className="w-full px-2 md:px-4 text-base md:text-xl text-[#BEC1DD]">{description}</p>
-      <div>
-
+      <div className="flex flex-col w-full items-center h-[40%]">
+        <h1 className="w-full p-2 md:p-4 text-xl md:text-3xl font-bold">
+          {title}
+        </h1>
+        <p className="w-full px-2 md:px-4 text-base md:text-xl text-[#BEC1DD]">
+          {description}
+        </p>
+      </div>
+      <div className="flex w-full  justify-between items-center pb-4">
+        <ul className="flex ">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+        <div className="text-gradient font-bold">
+          <a className="flex items-center" href="">
+            Check the site <GoArrowUpRight size={20} className="text-purple-600" />
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,34 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useScroll,
+  AnimatePresence,
+  useMotionValueEvent,
+} from "framer-motion";
 
 export const NavBar = () => {
   const [active, setActive] = useState<string | null>("Home");
   const links = ["Home", "About", "Projects", "Experience", "Contact"];
+
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(true);
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    if (typeof current === "number") {
+      let direction = current! - scrollYProgress.getPrevious()!;
+
+      if (scrollYProgress.get() < 0.05) {
+        setVisible(true);
+      } else {
+        if (direction < 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      }
+    }
+  });
 
   const handleClick = (link: string) => {
     setActive(link);
@@ -14,29 +39,29 @@ export const NavBar = () => {
   };
 
   return (
-    <motion.nav
-      initial="hidden"
-      animate="visible"
-      variants={{
-        hidden: { opacity: 0, x: -60 },
-        visible: { opacity: 1, x: 0 },
-      }}
-      transition={{ duration: 0.8 }}
-      className="flex fixed top-0 w-[92%] mt-10 max-w-[600px] h-[8%] justify-center items-center navbar-class border-1 border-[#272A3C] z-30"
-    >
-      <ul className="flex gap-4 md:gap-8 font-semibold">
-        {links.map((link) => (
-          <li
-            key={link}
-            className={`text-[#c1c2d3] text-sm md:text-xl cursor-pointer border-b-1 ${
-              active === link ? "border-white text-white" : "border-transparent"
-            }`}
-            onClick={() => handleClick(link)}
-          >
-            {link}
-          </li>
-        ))}
-      </ul>
-    </motion.nav>
+    <AnimatePresence mode="wait">
+      <motion.nav
+        initial ={{ opacity: 1, y: -100}}
+        animate={{ y: visible ? 0 : -100, opacity: visible ? 1 : 0}}
+        transition={{ duration: 0.5, }}
+        className="flex fixed top-0 w-[92%] mt-10 max-w-[600px] h-[8%] justify-center items-center navbar-class border-1 border-[#272A3C] z-30"
+      >
+        <ul className="flex gap-4 md:gap-8 font-semibold">
+          {links.map((link) => (
+            <li
+              key={link}
+              className={`text-[#c1c2d3] text-sm md:text-xl cursor-pointer border-b-1 ${
+                active === link
+                  ? "border-white text-white"
+                  : "border-transparent"
+              }`}
+              onClick={() => handleClick(link)}
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      </motion.nav>
+    </AnimatePresence>
   );
-}
+};
