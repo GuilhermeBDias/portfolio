d8176fe19ea8062b950efb7e38231fba89c0abc7
GuilhermeBDias <guilhermebdias55@gmail.com>
2025-09-26 12:45:30 -0300

    fix: animação resetada

--- src/components/FootBar.tsx
+++ src/components/FootBar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { socialLinks } from "../constants";
-import { Animated } from "../utils/AnimatedText";
 import { motion } from "framer-motion";
 import { containerVariants, itemVariants } from "../constants/animations";
 
@@ -8,7 +7,14 @@ const FootBar = () => {
   return (
     <div className="flex flex-col md:flex-row w-full gap-2 justify-center md:justify-between items-center pb-10">
       <div className="flex justify-center md:justify-start w-full">
-        <Animated>© 2025 Guilherme Dias. All rights reserved.</Animated>
+        <motion.h1
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={itemVariants}
+        >
+          © 2025 Guilherme Dias. All rights reserved.
+        </motion.h1>
       </div>
       <div className="w-full ">
         <motion.ul
@@ -28,7 +34,7 @@ const FootBar = () => {
                 href={social.link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="w-full h-full border-1 border-[#272A3C] rounded-lg  bg-[#05041F] p-2 hover:scale-105 transition-[scale]"
+                className="w-full h-full border-1 border-[#272A3C] rounded-lg bg-gradient-to-r from-[#05041F] to-[#0C0E23] p-2 hover:scale-105 transition-[scale]"
               >
                 {React.createElement(social.icon, { size: 24 })}
               </a>
